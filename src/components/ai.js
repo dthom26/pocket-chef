dcfe1e33c34ff658d9c254ce9786a86f739cdfde
Dallin Thomson <dallin.thomson@gmail.com>
2025-03-14 14:36:58 -0500

    adjusted ai prompt.

--- src/components/ai.js
+++ src/components/ai.js
@@ -1,7 +1,7 @@
 import { HfInference } from "@huggingface/inference";
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients and those additional ingredients should not be key ingredients. Format your response in markdown to make it easier to render to a web page
 `;
 
 const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN);
@@ -20,7 +20,8 @@ export async function getRecipeFromMistral(ingredientsArr) {
       ],
       max_tokens: 1024,
     });
-    return response.choices[0].message.content;
+    return response.choices[Math.floor(Math.random() * response.choices.length)]
+      .message.content;
   } catch (err) {
     console.error(err.message);
   }
